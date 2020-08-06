5426f6ea25fcba406b140395171117b192f1cc11
coret <coret@Willjrcom_Note>
2020-08-06 09:30:37 -0300

    busca por cep atualizado para https

--- src/main/resources/static/js/buscaCep.js
+++ src/main/resources/static/js/buscaCep.js
@@ -1,14 +1,32 @@
  	// Método para consultar o CEP
 	$('#cep').on('blur', function(){
- 
+
 		if($.trim($("#cep").val()) != ""){
-			$.getScript("http://cep.republicavirtual.com.br/web_cep.php?formato=javascript&cep="+$("#cep").val(), function(){
+			//$.getScript("http://cep.republicavirtual.com.br/web_cep.php?formato=javascript&cep="+$("#cep").val(), function(){
+			
+			$.ajax({
+				url: "https://viacep.com.br/ws/" + +$("#cep").val() + "/json/"
+				
+			}).done(function(e){
+				console.log(e);
+				$("#rua").val(e.logradouro);
+				$("#bairro").val(e.bairro);
+				$("#cidade").val(e.localidade);
+				
+			}).fail(function(){
+				$.alert("falhou!");
+			});
+			
+			/*
+			$.getScript("https://viacep.com.br/ws/" + +$("#cep").val() + "/json/", function(){
 			  	if(resultadoCEP["resultado"]){
 					$("#rua").val(unescape(resultadoCEP["tipo_logradouro"])+" "+unescape(resultadoCEP["logradouro"]));
 					$("#bairro").val(unescape(resultadoCEP["bairro"]));
 					$("#cidade").val(unescape(resultadoCEP["cidade"]));
 					$("#uf").val(unescape(resultadoCEP["uf"]));
 				}
-			});				
+			});			
+			*/	
 		}			
 	});
+	
