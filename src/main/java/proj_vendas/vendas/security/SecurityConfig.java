035ee4279770b4267b783e3230f5ec71866ead52
willjrcom <williamjunior67@gmail.com>
2020-09-06 02:39:33 -0300

    teste

--- src/main/java/proj_vendas/vendas/security/SecurityConfig.java
+++ src/main/java/proj_vendas/vendas/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/css/**", "/jquery/**", "/img/**", "/js/**", "/fonts/**", "/dev/**").permitAll()
+			.antMatchers("/css/**", "/jquery/**", "/img/**", "/js/**", "/fonts/**", "/dev/**", "/dev/criar/**").permitAll()
 			.anyRequest().authenticated()
 			
 			.and()
