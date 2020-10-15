f4adb82adc873dc92d65cc1ba5374a12ee30a3ba
willjrcom <williamjunior67@gmail.com>
2020-10-15 11:25:08 -0300

    Spring Security adicionado novamente

--- src/main/java/proj_vendas/vendas/security/SecurityConfig.java
+++ src/main/java/proj_vendas/vendas/security/SecurityConfig.java
@@ -1,6 +1,14 @@
 package proj_vendas.vendas.security;
 
-/*
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import proj_vendas.vendas.service.UsuarioService;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
@@ -34,4 +42,3 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
-*/
--- src/main/java/proj_vendas/vendas/service/UsuarioService.java
+++ src/main/java/proj_vendas/vendas/service/UsuarioService.java
@@ -1,6 +1,18 @@
 package proj_vendas.vendas.service;
 
-/*
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import proj_vendas.vendas.model.Usuario;
+import proj_vendas.vendas.repository.Usuarios;
+
 @Service
 public class UsuarioService implements UserDetailsService{
 	
@@ -33,4 +45,3 @@ public class UsuarioService implements UserDetailsService{
 	
 	
 }
-*/
