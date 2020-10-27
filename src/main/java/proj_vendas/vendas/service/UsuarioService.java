9e0178ad79fc856649b412226d82e5e981333cf3
willjrcom <williamjunior67@gmail.com>
2020-10-27 14:26:07 -0300

    abrir dia atual ao carregar login

--- src/main/java/proj_vendas/vendas/service/UsuarioService.java
+++ src/main/java/proj_vendas/vendas/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package proj_vendas.vendas.service;
 
+import java.time.LocalDate;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import proj_vendas.vendas.model.Dado;
+import proj_vendas.vendas.model.Dia;
 import proj_vendas.vendas.model.Usuario;
+import proj_vendas.vendas.repository.Dados;
+import proj_vendas.vendas.repository.Dias;
 import proj_vendas.vendas.repository.Usuarios;
 
 @Service
@@ -18,6 +24,12 @@ public class UsuarioService implements UserDetailsService{
 	
 	@Autowired
 	private Usuarios usuarios;
+
+	@Autowired
+	private Dados dados;
+	
+	@Autowired
+	private Dias dias;
 	
 	@Transactional
 	public Usuario buscarPorEmail(String email) {
@@ -28,6 +40,26 @@ public class UsuarioService implements UserDetailsService{
 	@Override @Transactional
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Usuario usuario = buscarPorEmail(username);
+		
+		LocalDate diaAtual = LocalDate.now(); // Create a date object
+		String dia = diaAtual.toString();
+	    
+		//alterar a tabela dados
+		Dado dado = new Dado();
+		dado.setData(dia);
+		
+		//alterar a tabela dia
+		Dia data = dias.buscarId1();
+		
+		if(data == null) {
+			data = new Dia();
+		}
+		data.setDia(dia);
+		
+		//salvar dados
+		dias.save(data);
+		dados.save(dado);
+		
 		return new User(
 				usuario.getEmail(),
 				usuario.getSenha(),
