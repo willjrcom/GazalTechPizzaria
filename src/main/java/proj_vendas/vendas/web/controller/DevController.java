39ea0767d3531163933ac08d7578383c1cd2edcf
willjrcom <williamjunior67@gmail.com>
2020-10-28 19:45:34 -0300

    Criação e validação de usuario completo

--- src/main/java/proj_vendas/vendas/web/controller/DevController.java
+++ src/main/java/proj_vendas/vendas/web/controller/DevController.java
@@ -1,19 +1,26 @@
 package proj_vendas.vendas.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import proj_vendas.vendas.model.Usuario;
+import proj_vendas.vendas.repository.Usuarios;
+
 @Controller
 @RequestMapping("/dev")
 public class DevController {
-	/*
+	
 	@Autowired
 	private Usuarios usuarios;
-	*/
+	
 	@RequestMapping
 	public ModelAndView tela() {
 		return new ModelAndView("dev");
@@ -22,18 +29,33 @@ public class DevController {
 	@RequestMapping(value = "/liberar/{codigo}", method = RequestMethod.PUT)
 	@ResponseBody
 	public boolean liberarCadastro(@PathVariable String codigo) {
-		if(codigo.equals("objetivo42461255")) {
+		if(codigo.equals("willjrcom18")) {
 			return true;
 		}else {
 			return false;
 		}
 	}
-	/*
+
 	@RequestMapping(value = "/criar", method = RequestMethod.PUT)
 	@ResponseBody
 	public Usuario criarUsuario(@RequestBody Usuario usuario) {
 		usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
 		return usuarios.save(usuario);
 	}
-	*/
+	
+	@RequestMapping(value = "/validar/{email}/{id}", method = RequestMethod.PUT)
+	@ResponseBody
+	public Usuario validar(@PathVariable String email, @ModelAttribute("id")Usuario usuario) {
+		Usuario busca = usuarios.findByEmail(email);
+		
+		if(busca != null) {
+			if(busca.getId() == usuario.getId()) {
+				Usuario vazio = new Usuario();
+				vazio.setId((long) -1);
+				return vazio;
+			}
+		}
+		
+		return usuarios.findByEmail(email);
+	}
 }
--- src/main/resources/static/js/dev.js
+++ src/main/resources/static/js/dev.js
@@ -2,20 +2,30 @@
 var codigo;
 var dados = {}, email, senha, confirmar;
 var linhaHtml = '<div class="form-group">'
-				+'<label>E-mail:<br/></label>'
+				+'<label>E-mail:<br></label>'
+				+'<p id="avisoUsuario">Usuário já cadastrado, tente outro!</p>'
 				+'<input type="email" class="form-control pula" id="email" name="email" placeholder="nome@exemplo.com" />'
 			+'</div>'
 				
 			+'<div class="row">'
 				+'<div class="form-group col-md-6">'
-					+'<label>Senha:<br/></label>'
+					+'<label>Senha:<br></label>'
+					+'<p id="avisoSenha">As senhas não conferem!</p>'
 					+'<input type="password" class="form-control pula" id="senha" name="senha" placeholder="Senha" />' 
 				+'</div>'
 	
 				+'<div class="form-group col-md-6">'
-					+'<label>Confirmar Senha:<br/></label>'
+					+'<label>Confirmar Senha:<br></label>'
 					+'<input type="password" class="form-control pula" id="confirmar" name="confirmar" placeholder="Confimar senha" />'  
 				+'</div>'
+			+'</div>'
+			
+			+'<div class="form-group">'
+				+'<label>Permissão:<br></label>'
+				+'<select class="form-control" id="perfil">'
+					+'<option value="USUARIO">USUARIO</option>'
+					+'<option value="ADM">ADM</option>'
+				+'</select>'
 			+'</div>'
 			
 			+'<br>'
@@ -30,13 +40,100 @@ $("#buscar").click(function(){
 		url:'/dev/liberar/' + codigo,
 		type: 'PUT'
 	}).done(function(e){
-		$("#liberar").html(linhaHtml);
-
+		if(e == true) {
+			$("#liberar").html(linhaHtml);
+			$("#divLiberacao").hide();
+		}
 		//---------------------------------------------------------
+		$("#avisoUsuario").hide();
+		$("#avisoSenha").hide();
+		$('#email').on('blur', function(){// Método para consultar o Usuario
+
+			if($("#email").val() != ''){
+				
+				var email = $("#email").val();
+				var id = $("#id").val();	
+				
+				$.ajax({
+					url:  (id != '') ? "/dev/validar/" + email.toString() + '/' + id : "/dev/validar/" + email.toString() + '/-2',
+					type: 'PUT'
+				}).done(function(event){
+
+					if(event.length != 0 && event != '' && event.id != -1) {
+						$("#avisoUsuario").show().css({
+							'color': 'red'
+						});
+						$("#email").css({
+							'border':'1px solid red'
+						});
+						$("#criar").hide();
+					}else {
+						$("#avisoUsuario").hide();
+						$("#criar").show();
+						$("#email").css({
+							'border':'1px solid #ccc'
+						});
+					}
+				});
+			}			
+		});
+		
+		$('#confirmar').on('blur', function(){//comparar senhas
+			if($(this).val() !== $("#senha").val()) {
+				$("#avisoSenha").show().css({
+					'color': 'red'
+				});
+				$("#senha").css({
+					'border':'1px solid red'
+				});
+				$("#confirmar").css({
+					'border':'1px solid red'
+				});
+				$("#criar").hide();
+			}else if($(this).val() === $("#senha").val()) {
+				$("#avisoSenha").hide();
+				$("#criar").show();
+				$("#senha").css({
+					'border':'1px solid #ccc'
+				});
+				$("#confirmar").css({
+					'border':'1px solid #ccc'
+				});
+			}
+		});
+		
+		
+		$("#senha").on('blur', function(){ //comparar senhas
+			if($(this).val() !== $("#confirmar").val()) {
+				$("#avisoSenha").show().css({
+					'color': 'red'
+				});
+				$("#senha").css({
+					'border':'1px solid red'
+				});
+				$("#confirmar").css({
+					'border':'1px solid red'
+				});
+				$("#criar").hide();
+			}else if($(this).val() === $("#confirmar").val()) {
+				$("#avisoSenha").hide();
+				$("#criar").show();
+				$("#senha").css({
+					'border':'1px solid #ccc'
+				});
+				$("#confirmar").css({
+					'border':'1px solid #ccc'
+				});
+			}
+		});
+		
+		
+		//-----------------------------------------------------------
 		$("#criar").click(function(){
 
 			dados.email = $("#email").val();
 			dados.senha = $("#senha").val();
+			dados.perfil = $("#perfil").val();
 			confirmar = $("#confirmar").val();
 			
 			if(dados.senha === confirmar && dados.senha != '' && dados.email != '') {
@@ -47,7 +144,21 @@ $("#buscar").click(function(){
 					contentType: "application/json",
 					data: JSON.stringify(dados)
 				}).done(function(e){
-					$.alert("foi");
+					$.alert({
+						type: 'blue',
+						title: 'Sucesso',
+						content: 'Usuário cadastrado!',
+						buttons: {
+							confirm:{
+								text:'menu',
+								btnClass: 'btn-success',
+								keys: ['enter', 'esc'],
+								action: function(){
+									window.location.href = "/menu";
+								}
+							}
+						}
+					});
 				}).fail(function(){
 					$.alert("Falhou");
 				})
