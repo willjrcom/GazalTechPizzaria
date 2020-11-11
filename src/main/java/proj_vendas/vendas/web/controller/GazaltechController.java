3d12126e25c450870310cb1045db4f670b8c99eb
willjrcom <williamjunior67@gmail.com>
2020-11-11 19:22:41 -0300

    teste

--- src/main/java/proj_vendas/vendas/web/controller/GazaltechController.java
+++ src/main/java/proj_vendas/vendas/web/controller/GazaltechController.java
@@ -1,15 +1,23 @@
 package proj_vendas.vendas.web.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import proj_vendas.vendas.model.Pedido;
+import proj_vendas.vendas.repository.Pedidos;
+
+@RestController
 @RequestMapping("/gazaltech")
 public class GazaltechController {
 
+	@Autowired
+	private Pedidos pedidos;
+	
 	@RequestMapping
-	public String imprimir() {
-		return "sucesso no acesso";
+	public List<Pedido> imprimir() {
+		return pedidos.findAll();
 	}
 }
