5e9a129e79cdd25d7be96764fbbfe3c3cd1196ec
willjrcom <williamjunior67@gmail.com>
2021-02-17 10:21:33 -0300

    Tela adicionada

---
+++ src/main/java/proj_vendas/vendas/web/controller/NovoPedido/SaidaBarController.java
@@ -0,0 +1,41 @@
+package proj_vendas.vendas.web.controller.NovoPedido;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import proj_vendas.vendas.model.PedidoTemp;
+import proj_vendas.vendas.model.Usuario;
+import proj_vendas.vendas.repository.PedidoTemps;
+import proj_vendas.vendas.repository.Usuarios;
+
+@Controller
+@RequestMapping("/saidaBar")
+public class SaidaBarController{
+	
+	@Autowired
+	private PedidoTemps temps;
+
+	@Autowired
+	private Usuarios usuarios;
+
+	@RequestMapping
+	public ModelAndView pronto() {
+		return new ModelAndView("saidaBar");
+	}
+	
+	@RequestMapping(value = "/todosPedidos")
+	@ResponseBody
+	public List<PedidoTemp> todosPedidos() {
+		Usuario user = usuarios.findByEmail(((UserDetails)SecurityContextHolder.getContext()
+				.getAuthentication().getPrincipal()).getUsername());
+		
+		return temps.findByCodEmpresaAndSetor(user.getCodEmpresa(), 2); //mostrar todos
+	}
+}
