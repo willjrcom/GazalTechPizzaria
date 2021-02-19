19866ecbc49152ddb834e76405c0462d4ec5656b
willjrcom <williamjunior67@gmail.com>
2021-02-18 21:06:01 -0300

    nova alteração

--- src/main/java/proj_vendas/vendas/web/controller/NovaSenhaController.java
+++ src/main/java/proj_vendas/vendas/web/controller/NovaSenhaController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,17 +21,21 @@ public class NovaSenhaController {
 
 	@Autowired
 	private Usuarios usuarios;
-	
-	@RequestMapping("/auth/6sf465sd4f5d4g6v8d5f4gv6dx5f4g6rt4h6/8tygh4rt8d5t4r68ft4g68rrft4ge9r5gh43tf/f435t4h24gg55xd5f4g5ft4ert54/{email}/d53y54grd5fy4gr35tf4ygrt54fyg6rt54yh68rt5yfg")
-    public void telaNovaSenha(@PathVariable String email) {
-    	tela(email);
-    }
-	
-	public ModelAndView tela(String email) {
+
+	@RequestMapping
+	public ModelAndView tela() {
 		ModelAndView mv = new ModelAndView("novaSenha");
-    	mv.addObject("email", email);
+    	mv.addObject("email", "Nenhum email encontrado!");
     	return mv;
 	}
+	
+	
+	@RequestMapping("/auth/6sf465sd4f5d4g6v8d5f4gv6dx5f4g6rt4h6/8tygh4rt8d5t4r68ft4g68rrft4ge9r5gh43tf/f435t4h24gg55xd5f4g5ft4ert54/{email}/d53y54grd5fy4gr35tf4ygrt54fyg6rt54yh68rt5yfg")
+    public String telaNovaSenha(@PathVariable String email, ModelMap model) {
+		model.addAttribute("email", email);
+    	return "novaSenha";
+    }
+	
 	
 	
 	@RequestMapping("/criar")
